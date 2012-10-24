fdda06961b476c9ddc32e015d4baf8e9452880d8
Kent Dodds <me@kentcdodds.com>
2012-10-24 16:58:46 -0600

    Updating...

--- src/com/kentcdodds/flashlight/MainActivity.java
+++ src/com/kentcdodds/flashlight/MainActivity.java
@@ -12,10 +12,8 @@ public class MainActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
     setContentView(com.kentcdodds.flashlight.R.layout.main);
   }
